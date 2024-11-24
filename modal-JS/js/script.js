b0cc94e9eb2e201bb38be6d8370c60e20aaeed87
glenda. <126438315+glendamara@users.noreply.github.com>
2024-11-24 12:05:39 -0300

    Add files via upload

---
+++ modal-JS/js/script.js
@@ -0,0 +1,10 @@
+const openButtons = document.querySelectorAll('.open-modal');
+
+openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+        modal.showModal();
+    });
+});
